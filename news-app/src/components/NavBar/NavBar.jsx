3f6436fae106bc60921c0f6788ced012616f8932
Amir <khan.ayyub@happiestminds.com>
2022-05-01 14:13:35 +0530

    added favourite count

--- news-app/src/components/NavBar/NavBar.jsx
+++ news-app/src/components/NavBar/NavBar.jsx
@@ -2,21 +2,29 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import SearchBox from "./SearchBox/SearchBox";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { FAV_DATA } from "../../context/article-slice";
+import { useSelector } from "react-redux";
 
 export default function NavBar() {
+  const favCount = useSelector(FAV_DATA).length;
+
   return (
     <Navbar bg="primary" expand="lg" className="nav__container">
       <Container>
         <Navbar.Brand style={{ color: "white", marginRight: "2rem" }}>
           <h4>News App</h4>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle className="side_bar" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavLink style={{ marginRight: "2rem" }} to="/">
               Home
             </NavLink>
-            <NavLink to="/favourites">Favourites</NavLink>
+            <NavLink to="/favourites">
+              Favourites &nbsp;
+              <span className="fav_count">{favCount}</span>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
