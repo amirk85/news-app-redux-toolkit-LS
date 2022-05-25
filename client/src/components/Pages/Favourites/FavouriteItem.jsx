ccb4bc1cde9e2d5b61437358730d16fb0f8d8495
Amir <khan.ayyub@happiestminds.com>
2022-05-25 10:11:18 +0530

    .

--- client/src/components/Pages/Favourites/FavouriteItem.jsx
+++ client/src/components/Pages/Favourites/FavouriteItem.jsx
@@ -6,32 +6,15 @@ import Typography from "@mui/material/Typography";
 import { Button, CardActions } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { REMOVE_FAV } from "../../../store/article-slice";
-import Swal from "sweetalert2";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
-import { useNavigate } from "react-router-dom";
 import "./FavouriteItem.css";
 
 export default function FavouriteItem({ article }) {
   const { author, title, url, urlToImage } = article;
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   function removeFavHandler(url) {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Confirm",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire("Removed!", "Removed From Favourites", "success");
-        dispatch(REMOVE_FAV(url));
-        navigate("/favourites");
-      }
-    });
+    dispatch(REMOVE_FAV(url));
   }
 
   return (
